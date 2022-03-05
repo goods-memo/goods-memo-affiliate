94f6cc9f5b1ccef53b15dbddc8a48d4ff74baaf8
kijim <kijim@MOUSE-COMPUTER>
2022-03-05 19:01:04 +0900

    作成中　class FieldInfo

---
+++ wp-content/plugins/goods-memo-affiliate/option/field/SelectFieldInfo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace goodsmemo\option\field;
+
+use goodsmemo\option\field\AbstractFieldInfo;
+
+require_once GOODS_MEMO_DIR . "option/field/AbstractFieldInfo.php";
+
+/**
+ *
+ * @author kijim
+ *        
+ */
+class SelectFieldInfo extends AbstractFieldInfo {
+}
+
