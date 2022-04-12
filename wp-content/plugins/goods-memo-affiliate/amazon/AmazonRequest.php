c13997874f11375178db49195bcece0be5f99a96
kijim <kijim@MOUSE-COMPUTER>
2022-04-12 17:58:53 +0900

    AWS SDKなしで、商品情報をリクエスト
    XAMPPローカル環境で、401 InvalidSignatureが出る不具合に対応するため

---
+++ wp-content/plugins/goods-memo-affiliate/amazon/AmazonRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace goodsmemo\amazon;
+
+use goodsmemo\amazon\CommonRESTParameter;
+use goodsmemo\amazon\RESTParameter;
+use goodsmemo\amazon\withoutsdk\RequestSearchIndex;
+use goodsmemo\network\URLInfo;
+use goodsmemo\exception\HttpRequestException;
+
+require_once GOODS_MEMO_DIR . "amazon/CommonRESTParameter.php";
+require_once GOODS_MEMO_DIR . "amazon/RESTParameter.php";
+require_once GOODS_MEMO_DIR . "amazon/withoutsdk/RequestSearchIndex.php";
+require_once GOODS_MEMO_DIR . "network/URLInfo.php";
+require_once GOODS_MEMO_DIR . "exception/HttpRequestException.php";
+
+class AmazonRequest {
+
+	public static function requestSearchIndex(URLInfo $urlInfo, CommonRESTParameter $commonParameter, RESTParameter $restParameter, int $itemCount, $retryCount = 3) {
+
+		if ($itemCount <= 0) {
+			return NULL; // 表示する件数が０件なので、商品情報なしとする。
+		}
+
+		$partnerTag = $commonParameter->getAssociateTag ();
+		$keyword = $restParameter->getKeyword ();
+		$searchIndex = $restParameter->getSearchIndex ();
+		$resources = $restParameter->getSearchItemsResources ();
+		$hostname = $urlInfo->getHostname ();
+		$accessKey = $commonParameter->getAccessKey ();
+		$secretKey = $commonParameter->getSecretKey ();
+		$regionName = $commonParameter->getRegion ();
+
+		$lastHttpRequestException;
+		for($i = 0; $i < $retryCount; $i ++) { // リトライ回数：3回 //TODO 設定画面で指定する
+			if ($i >= 1) {
+				sleep ( 1 ); // 再試行の待ち時間（1秒）
+			}
+
+			try {
+
+				$searchItemsResponse = RequestSearchIndex::requestSearchIndex ( $partnerTag, $keyword, $searchIndex, $resources, $hostname, $accessKey, $secretKey, $regionName );
+
+				return $searchItemsResponse;
+			} catch ( HttpRequestException $ex ) {
+				$lastHttpRequestException = $ex;
+			}
+		}
+
+		throw $lastHttpRequestException;
+	}
+}
+	
