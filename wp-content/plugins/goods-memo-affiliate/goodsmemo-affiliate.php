7cf81cd487a93128a7a6fff1d0fb2b8a157a7310
kijim <kijim@MOUSE-COMPUTER>
2022-04-14 11:34:57 +0900

    Version: 0.3.0

--- wp-content/plugins/goods-memo-affiliate/goodsmemo-affiliate.php
+++ wp-content/plugins/goods-memo-affiliate/goodsmemo-affiliate.php
@@ -4,29 +4,10 @@
  * Plugin Name: Goods Memo Affiliate
  * Plugin URI:
  * Description: アフィリエイトの商品を表示します。
- * Version: 0.2.1
+ * Version: 0.3.0
  * Author:Goods Memo
  * Author URI: https://www.goods-memo.net/goods-memo-affiliate/
  * License: GPL v2 or later
- */
-
-/*
- * Copyright (C) 2018 Goods Memo.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301 USA
  */
 define ( "GOODS_MEMO_DIR", __DIR__ . "/" );
 define ( "GOODS_MEMO_PREFIX", "goodsmemo" );
