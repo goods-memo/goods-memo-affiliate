1e8b3e181bc13875ae54df4cc3ea0789d691f188
admin <admin@DESKTOP-L5SU05J>
2022-11-11 21:16:23 +0900

    Version: 0.3.6

--- wp-content/plugins/goods-memo-affiliate/goodsmemo-affiliate.php
+++ wp-content/plugins/goods-memo-affiliate/goodsmemo-affiliate.php
@@ -4,7 +4,7 @@
  * Plugin Name: Goods Memo Affiliate
  * Plugin URI:
  * Description: アフィリエイトの商品を表示します。
- * Version: 0.3.5
+ * Version: 0.3.6
  * Author:Goods Memo
  * Author URI: https://www.goods-memo.net/computers/goods-memo-affiliate/
  * License: GPL v2 or later
