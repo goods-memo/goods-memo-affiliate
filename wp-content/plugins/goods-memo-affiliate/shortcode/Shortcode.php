592b7868559049d46b2d759168a48aa487308c10
admin <admin@DESKTOP-L5SU05J>
2023-03-04 21:27:21 +0900

    if文をtry文の前に移動した

--- wp-content/plugins/goods-memo-affiliate/shortcode/Shortcode.php
+++ wp-content/plugins/goods-memo-affiliate/shortcode/Shortcode.php
@@ -27,27 +27,26 @@ class Shortcode {
 		 * 特にハイフン（ダッシュ）には注意して、使わないのが賢明です。
 		 * 注意: 属性名は大文字と小文字が混在可能ですが、パース後はいつも小文字になります。
 		 */
-		$attsMap = shortcode_atts ( array ( // 変数名（属性名） => 初期値
-				"service" => "",
-				"operation" => "",
-				"search_index" => "",
-				"keyword" => "",
-				"number" => "",
-				"item_title_length" => "",
-				"item_review_length" => ""
-		), $atts );
+		$attsMap = shortcode_atts ( 
+				array ( // 変数名（属性名） => 初期値
+				"service" => "","operation" => "","search_index" => "","keyword" => "",
+						"number" => "","item_title_length" => "",
+						"item_review_length" => ""
+				), $atts );
 		extract ( $attsMap ); // 例：変数 $service などを作成する
 
+		if ($number == 0) {
+
+			$message = <<< EOD
+			<p class="gma-zero-ads-displayed-message">広告はありません（表示件数の設定[{$number}]件）。</p>
+			EOD;
+			return $message;
+		}
+
 		try {
-			$shortcodeAttribute = ShortcodeAttributeUtils::makeShortcodeAttribute ( $operation, $search_index, $keyword, $number, $item_title_length, $item_review_length );
-
-			if ($number == 0) {
-
-				$message = <<< EOD
-				<p class="gma-zero-ads-displayed-message">広告はありません（表示件数の設定{$number}件）。</p>
-				EOD;
-				return $message;
-			}
+			$shortcodeAttribute = ShortcodeAttributeUtils::makeShortcodeAttribute ( 
+					$operation, $search_index, $keyword, $number, $item_title_length,
+					$item_review_length );
 
 			$affiliateHTML;
 			switch ($service) {
@@ -73,15 +72,18 @@ class Shortcode {
 			return $message;
 		} catch ( OptionException $ex ) {
 
-			$message = '<p class="gma-error-message">オプションデータベースの例外：' . $ex->getMessage () . '</p>';
+			$message = '<p class="gma-error-message">オプションデータベースの例外：' . $ex->getMessage () .
+					'</p>';
 			return $message;
 		} catch ( HttpRequestException $ex ) {
 
-			$message = '<p class="gma-error-message">HTTPリクエストの例外：' . $ex->getMessage () . '。コード：' . $ex->getCode () . '</p>';
+			$message = '<p class="gma-error-message">HTTPリクエストの例外：' . $ex->getMessage () .
+					'。コード：' . $ex->getCode () . '</p>';
 			return $message;
 		} catch ( HttpResponseException $ex ) {
 
-			$message = '<p class="gma-error-message">HTTPレスポンスの例外：' . $ex->getMessage () . '</p>';
+			$message = '<p class="gma-error-message">HTTPレスポンスの例外：' . $ex->getMessage () .
+					'</p>';
 			return $message;
 		} catch ( \Exception $ex ) {
 			// \Exceptionをキャッチすれば、WordPressの「サイトに技術的な問題が発生しています。」を防げるかも？？
