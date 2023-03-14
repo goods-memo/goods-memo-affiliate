15f3f7cd3a60603c6be598b5f4e6264d3d860c0f
admin <admin@DESKTOP-L5SU05J>
2023-03-14 12:57:43 +0900

    コメント文を削除した

--- wp-content/plugins/goods-memo-affiliate/amazon/displayhtml/DisplayHTMLPAAPINotAvailableOption.php
+++ wp-content/plugins/goods-memo-affiliate/amazon/displayhtml/DisplayHTMLPAAPINotAvailableOption.php
@@ -2,46 +2,25 @@
 
 namespace goodsmemo\amazon\displayhtml;
 
-/**
- *
- * @author kijim
- *        
- */
 class DisplayHTMLPAAPINotAvailableOption {
 	private string $displayHTML = "";
 	private bool $displayHTMLAlwaysEnabled = false;
 
-	/**
-	 *
-	 * @return string
-	 */
 	public function getDisplayHTMLPAAPINotAvailable(): string {
 
 		return $this->displayHTML;
 	}
 
-	/**
-	 *
-	 * @param string $displayHTML
-	 */
 	public function setDisplayHTMLPAAPINotAvailable(string $displayHTML) {
 
 		$this->displayHTML = $displayHTML;
 	}
 
-	/**
-	 *
-	 * @return boolean
-	 */
 	public function getDisplayHTMLPAAPINotAvailableAlwaysEnabled(): bool {
 
 		return $this->displayHTMLAlwaysEnabled;
 	}
 
-	/**
-	 *
-	 * @param boolean $displayHTMLAlwaysEnabled
-	 */
 	public function setDisplayHTMLPAAPINotAvailableAlwaysEnabled(bool $displayHTMLAlwaysEnabled) {
 
 		$this->displayHTMLAlwaysEnabled = $displayHTMLAlwaysEnabled;
