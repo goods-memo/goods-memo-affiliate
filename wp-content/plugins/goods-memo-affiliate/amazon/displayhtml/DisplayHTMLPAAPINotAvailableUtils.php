961b058edab618771230fd2a8ebca5508875f5b3
admin <admin@DESKTOP-L5SU05J>
2023-03-14 12:45:27 +0900

    コメント文を削除した

--- wp-content/plugins/goods-memo-affiliate/amazon/displayhtml/DisplayHTMLPAAPINotAvailableUtils.php
+++ wp-content/plugins/goods-memo-affiliate/amazon/displayhtml/DisplayHTMLPAAPINotAvailableUtils.php
@@ -14,21 +14,15 @@ require_once GOODS_MEMO_DIR . "amazon/displayhtml/DisplayHTMLPAAPINotAvailableOp
 require_once GOODS_MEMO_DIR . "amazon/AmazonItemsHTMLInfoMaker.php";
 require_once GOODS_MEMO_DIR . "item/html/ItemHTMLOption.php";
 
-/**
- *
- * @author kijim
- *        
- */
 class DisplayHTMLPAAPINotAvailableUtils {
 	private const DISPLAY_HTML_PAAPI_NOT_AVAILABLE_PREFIX = "display_html";
 
-	public static function makeDisplayHTMLPAAPINotAvailable(CommonRESTParameter $commonParameter, RESTParameter $restParameter, DisplayHTMLPAAPINotAvailableOption $displayHTMLOption) {
+	public static function makeDisplayHTMLPAAPINotAvailable(CommonRESTParameter $commonParameter,
+			RESTParameter $restParameter, DisplayHTMLPAAPINotAvailableOption $displayHTMLOption) {
 
 		$displayHTML = $displayHTMLOption->getDisplayHTMLPAAPINotAvailable ();
 
-		$searchArray = array (
-				"GOODS_MEMO_ASSOCIATE_TAG",
-				"GOODS_MEMO_SEARCH_KEYWORD",
+		$searchArray = array ("GOODS_MEMO_ASSOCIATE_TAG","GOODS_MEMO_SEARCH_KEYWORD",
 				"GOODS_MEMO_ENCODED_SEARCH_KEYWORD"
 		);
 
@@ -36,10 +30,7 @@ class DisplayHTMLPAAPINotAvailableUtils {
 		$keyword = $restParameter->getKeyword ();
 		$encoded_keyword = rawurlencode ( $keyword );
 
-		$replaceArray = array (
-				$associateTag,
-				$keyword,
-				$encoded_keyword
+		$replaceArray = array ($associateTag,$keyword,$encoded_keyword
 		);
 
 		$newDisplayHTML = str_replace ( $searchArray, $replaceArray, $displayHTML );
@@ -47,32 +38,38 @@ class DisplayHTMLPAAPINotAvailableUtils {
 		return $newDisplayHTML;
 	}
 
-	public static function geDisplayHTMLPAAPINotAvailableCache(ItemHTMLOption $itemHTMLOption, AmazonItemsHTMLInfoMaker $itemsHTMLInfoMaker) {
+	public static function geDisplayHTMLPAAPINotAvailableCache(ItemHTMLOption $itemHTMLOption,
+			AmazonItemsHTMLInfoMaker $itemsHTMLInfoMaker) {
 
 		$cacheExpirationInSeconds = $itemHTMLOption->getCacheExpirationInSeconds ();
 		if ($cacheExpirationInSeconds <= 0) {
 			return false;
 		}
 
-		$transientID = DisplayHTMLPAAPINotAvailableUtils::makeTransientID ( $itemHTMLOption, $itemsHTMLInfoMaker );
+		$transientID = DisplayHTMLPAAPINotAvailableUtils::makeTransientID ( $itemHTMLOption,
+				$itemsHTMLInfoMaker );
 		$displayHTMLCache = get_transient ( $transientID );
 
 		return $displayHTMLCache;
 	}
 
-	public static function setDisplayHTMLPAAPINotAvailableCache(string $displayHTML, ItemHTMLOption $itemHTMLOption, AmazonItemsHTMLInfoMaker $itemsHTMLInfoMaker) {
+	public static function setDisplayHTMLPAAPINotAvailableCache(string $displayHTML,
+			ItemHTMLOption $itemHTMLOption, AmazonItemsHTMLInfoMaker $itemsHTMLInfoMaker) {
 
-		$transientID = DisplayHTMLPAAPINotAvailableUtils::makeTransientID ( $itemHTMLOption, $itemsHTMLInfoMaker );
+		$transientID = DisplayHTMLPAAPINotAvailableUtils::makeTransientID ( $itemHTMLOption,
+				$itemsHTMLInfoMaker );
 		$cacheExpirationInSeconds = $itemHTMLOption->getCacheExpirationInSeconds ();
 
 		// 一時的にデータベースに「Product Advertising API 利用不可の時、表示するHTML」を保存する。有効期限後、削除される。
 		set_transient ( $transientID, $displayHTML, $cacheExpirationInSeconds );
 	}
 
-	private static function makeTransientID(ItemHTMLOption $itemHTMLOption, AmazonItemsHTMLInfoMaker $itemsHTMLInfoMaker) {
+	private static function makeTransientID(ItemHTMLOption $itemHTMLOption,
+			AmazonItemsHTMLInfoMaker $itemsHTMLInfoMaker) {
 
 		$uniqueText = $itemsHTMLInfoMaker->makeUniqueText ( $itemHTMLOption );
-		$uniqueTextOfDisplayHTML = DisplayHTMLPAAPINotAvailableUtils::DISPLAY_HTML_PAAPI_NOT_AVAILABLE_PREFIX . $uniqueText;
+		$uniqueTextOfDisplayHTML = DisplayHTMLPAAPINotAvailableUtils::DISPLAY_HTML_PAAPI_NOT_AVAILABLE_PREFIX .
+				$uniqueText;
 
 		// $transientID：キャッシュされるデータにつけるユニークな識別子。長さ 45 文字以下であること。
 		// md5() ：32 文字の 16 進数からなるハッシュを返します。
