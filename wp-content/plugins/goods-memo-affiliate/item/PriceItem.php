f2edab8c481f269f35ac3e04cc2ae73cd9ef6136
admin <admin@DESKTOP-L5SU05J>
2023-03-28 17:16:02 +0900

    コメント文を削除した

--- wp-content/plugins/goods-memo-affiliate/item/PriceItem.php
+++ wp-content/plugins/goods-memo-affiliate/item/PriceItem.php
@@ -1,77 +1,61 @@
 <?php
 
-/*
- * Copyright (C) 2018 Goods Memo.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
- */
-
 namespace goodsmemo\item;
 
-/**
- * Description of PriceItem
- *
- * @author Goods Memo
- */
 class PriceItem {
+	private $label = "";
+	private $price = "";
+	private $priceAddition = "";
+	private $priceTime = 0;
+	private $postageText = "";
 
-    private $label = "";
-    private $price = "";
-    private $priceAddition = "";
-    private $priceTime = 0;
-    private $postageText = "";
+	public function getLabel() {
 
-    public function getLabel() {
-	return $this->label;
-    }
+		return $this->label;
+	}
 
-    public function setLabel($label) {
-	$this->label = $label;
-    }
+	public function setLabel($label) {
 
-    public function getPrice() {
-	return $this->price;
-    }
+		$this->label = $label;
+	}
 
-    public function setPrice($price) {
-	$this->price = $price;
-    }
+	public function getPrice() {
 
-    public function getPriceAddition() {
-	return $this->priceAddition;
-    }
+		return $this->price;
+	}
 
-    public function setPriceAddition($priceAddition) {
-	$this->priceAddition = $priceAddition;
-    }
+	public function setPrice($price) {
 
-    public function getPriceTime(): float {
-	return $this->priceTime;
-    }
+		$this->price = $price;
+	}
 
-    public function setPriceTime(float $priceTime) {
-	$this->priceTime = $priceTime;
-    }
+	public function getPriceAddition() {
 
-    public function getPostageText() {
-	return $this->postageText;
-    }
+		return $this->priceAddition;
+	}
 
-    public function setPostageText($postageText) {
-	$this->postageText = $postageText;
-    }
+	public function setPriceAddition($priceAddition) {
 
+		$this->priceAddition = $priceAddition;
+	}
+
+	public function getPriceTime(): float {
+
+		return $this->priceTime;
+	}
+
+	public function setPriceTime(float $priceTime) {
+
+		$this->priceTime = $priceTime;
+	}
+
+	public function getPostageText() {
+
+		return $this->postageText;
+	}
+
+	public function setPostageText($postageText) {
+
+		$this->postageText = $postageText;
+	}
 }
