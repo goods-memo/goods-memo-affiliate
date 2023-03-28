c5e7ac82c4c0abf28c181c4b1b9abcb5c72e13a0
admin <admin@DESKTOP-L5SU05J>
2023-03-28 14:43:25 +0900

    コメント文を削除した

--- wp-content/plugins/goods-memo-affiliate/item/Item.php
+++ wp-content/plugins/goods-memo-affiliate/item/Item.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
- * Copyright (C) 2018 Goods Memo.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
- */
-
 namespace goodsmemo\item;
 
 use goodsmemo\item\ImageItem;
@@ -33,101 +14,113 @@ require_once GOODS_MEMO_DIR . "item/PointItem.php";
 require_once GOODS_MEMO_DIR . "item/ProductionItem.php";
 require_once GOODS_MEMO_DIR . "item/ReviewItem.php";
 
-/**
- * Description of Item
- *
- * @author Goods Memo
- */
 class Item {
-
 	private $pageURL = "";
 	private $imageItem;
 	private $title = "";
 	private $priceItem;
 	private $pointItem;
 	private $productionItem;
-	private $preferentialMember = ""; //優待会員
+	private $preferentialMember = ""; // 優待会員
 	private $shop = "";
 	private $reviewItem;
 
 	function __construct() {
-		$this->imageItem = new ImageItem();
-		$this->priceItem = new PriceItem();
-		$this->pointItem = new PointItem();
-		$this->productionItem = new ProductionItem(); //例：楽天で設定しない場合、空のProductionItemオブジェクトを使う。nullエラーを防げる。
-		$this->reviewItem = new ReviewItem();
+
+		$this->imageItem = new ImageItem ();
+		$this->priceItem = new PriceItem ();
+		$this->pointItem = new PointItem ();
+		$this->productionItem = new ProductionItem (); // 例：楽天で設定しない場合、空のProductionItemオブジェクトを使う。nullエラーを防げる。
+		$this->reviewItem = new ReviewItem ();
 	}
 
 	public function getPageURL() {
+
 		return $this->pageURL;
 	}
 
 	public function setPageURL($pageURL) {
+
 		$this->pageURL = $pageURL;
 	}
 
 	public function getImageItem(): ImageItem {
+
 		return $this->imageItem;
 	}
 
 	public function setImageItem(ImageItem $imageItem) {
+
 		$this->imageItem = $imageItem;
 	}
 
 	public function getTitle() {
+
 		return $this->title;
 	}
 
 	public function setTitle($title) {
+
 		$this->title = $title;
 	}
 
 	public function getPriceItem(): PriceItem {
+
 		return $this->priceItem;
 	}
 
 	public function setPriceItem(PriceItem $priceItem) {
+
 		$this->priceItem = $priceItem;
 	}
 
 	public function getPointItem(): PointItem {
+
 		return $this->pointItem;
 	}
 
 	public function setPointItem(PointItem $pointItem) {
+
 		$this->pointItem = $pointItem;
 	}
 
 	public function getProductionItem(): ProductionItem {
+
 		return $this->productionItem;
 	}
 
 	public function setProductionItem(ProductionItem $productionItem) {
+
 		$this->productionItem = $productionItem;
 	}
 
 	public function getPreferentialMember() {
+
 		return $this->preferentialMember;
 	}
 
 	public function setPreferentialMember($preferentialMember) {
+
 		$this->preferentialMember = $preferentialMember;
 	}
 
 	public function getShop() {
+
 		return $this->shop;
 	}
 
 	public function setShop($shop) {
+
 		$this->shop = $shop;
 	}
 
 	public function getReviewItem(): ReviewItem {
+
 		return $this->reviewItem;
 	}
 
 	public function setReviewItem(ReviewItem $reviewItem) {
+
 		$this->reviewItem = $reviewItem;
 	}
-
 }
