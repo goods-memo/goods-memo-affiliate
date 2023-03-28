31f0567fa83bf02ee90c72b9ab2ea9974751940a
admin <admin@DESKTOP-L5SU05J>
2023-03-28 17:44:10 +0900

    コメント文を削除した

--- wp-content/plugins/goods-memo-affiliate/item/PointItem.php
+++ wp-content/plugins/goods-memo-affiliate/item/PointItem.php
@@ -1,60 +1,41 @@
 <?php
 
-/*
- * Copyright (C) 2018 Goods Memo.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
- */
-
 namespace goodsmemo\item;
 
-/**
- * Description of PointItem
- *
- * @author Goods Memo
- */
 class PointItem {
 
-    //今の所、楽天だけで使うアイテムクラス。
-    private $pointRate = "";
-    private $pointRateStartTime = "";
-    private $pointRateEndTime = "";
+	// 今の所、楽天だけで使うアイテムクラス。
+	private $pointRate = "";
+	private $pointRateStartTime = "";
+	private $pointRateEndTime = "";
 
-    public function getPointRate() {
-	return $this->pointRate;
-    }
+	public function getPointRate() {
 
-    public function getPointRateStartTime() {
-	return $this->pointRateStartTime;
-    }
+		return $this->pointRate;
+	}
 
-    public function getPointRateEndTime() {
-	return $this->pointRateEndTime;
-    }
+	public function getPointRateStartTime() {
 
-    public function setPointRate($pointRate) {
-	$this->pointRate = $pointRate;
-    }
+		return $this->pointRateStartTime;
+	}
 
-    public function setPointRateStartTime($pointRateStartTime) {
-	$this->pointRateStartTime = $pointRateStartTime;
-    }
+	public function getPointRateEndTime() {
 
-    public function setPointRateEndTime($pointRateEndTime) {
-	$this->pointRateEndTime = $pointRateEndTime;
-    }
+		return $this->pointRateEndTime;
+	}
 
+	public function setPointRate($pointRate) {
+
+		$this->pointRate = $pointRate;
+	}
+
+	public function setPointRateStartTime($pointRateStartTime) {
+
+		$this->pointRateStartTime = $pointRateStartTime;
+	}
+
+	public function setPointRateEndTime($pointRateEndTime) {
+
+		$this->pointRateEndTime = $pointRateEndTime;
+	}
 }
