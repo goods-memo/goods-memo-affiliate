3e90efcdcd4c75da57b61067c730f1d6b44f74c0
admin <admin@DESKTOP-L5SU05J>
2023-04-10 16:57:25 +0900

    コメント文を書いた

--- wp-content/plugins/goods-memo-affiliate/item/ProductionItem.php
+++ wp-content/plugins/goods-memo-affiliate/item/ProductionItem.php
@@ -3,12 +3,16 @@
 namespace goodsmemo\item;
 
 class ProductionItem {
+	/* 投稿者（「役柄」と「人物名」） */
 	private $contributorArray = array (); // 空のオブジェクト作成に対応するため、空の配列を設定する。
+
+	/* 製造者（製造元、メーカー） */
 	private $manufacturerLabel = "";
-	private $manufacturer = ""; // 製造元、メーカー
+	private $manufacturer = "";
+
+	/* 種別（装丁、形式） */
 	private $binding = "";
 
-	// 装丁、形式、種別
 	public function getContributorArray() {
 
 		return $this->contributorArray;
