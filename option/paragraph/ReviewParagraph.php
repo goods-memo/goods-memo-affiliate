b374a209873f58d6141b299b379cf7da9893b869
programming goodsmemo <info.programming@goods-memo.net>
2023-05-29 13:02:30 +0900

    項目名を変更

--- option/paragraph/ReviewParagraph.php
+++ option/paragraph/ReviewParagraph.php
@@ -16,14 +16,15 @@ require_once GOODS_MEMO_DIR . "option/SectionInfo.php";
 require_once GOODS_MEMO_DIR . "option/field/TextField.php";
 require_once GOODS_MEMO_DIR . "option/field/TextareaField.php";
 
-class ReviewParagraph extends AbstractParagraph {
+class ReviewParagraph extends AbstractParagraph
+{
 	use AbstractTextParagraph;
 	const DEFAULT_EDITORIAL_REVIEW_LENGTH_LABEL = "商品説明の表示文字数（目安の文字数）";
 	const DEFAULT_EDITORIAL_REVIEW_LENGTH_VALUE = 120;
 	const DEFAULT_STRING_TO_DELETE_JSON_ARRAY_LABEL = "商品説明から削除する文字列のJSON配列（空の配列[]）";
 	// 削除する文字列の配列。例：全角の＜＞
 	const DEFAULT_STRING_TO_DELETE_JSON_ARRAY_VALUE = '["＜p＞","＜/p＞","＜b＞","＜br＞","＜br /＞"]';
-	const DEFAULT_STRING_TO_BREAK_JSON_OBJECT_LABEL = "商品説明で改行する文字のJSONオブジェクト（空のオブジェクト{}）";
+	const DEFAULT_STRING_TO_BREAK_JSON_OBJECT_LABEL = "商品説明で改行する文字列のJSONオブジェクト（空のオブジェクト{}）";
 	// 改行する文字列の配列
 	const DEFAULT_STRING_TO_BREAK_JSON_OBJECT_VALUE = <<< EOD
 	{
@@ -36,30 +37,38 @@ class ReviewParagraph extends AbstractParagraph {
 	}
 	EOD;
 
-	public function initParagraph(PageInfo $pageInfo, SectionInfo $sectionInfo, $fieldInfoArray) {
+	public function initParagraph(PageInfo $pageInfo, SectionInfo $sectionInfo, $fieldInfoArray)
+	{
 
-		parent::setOptionGroup ( $pageInfo->getOptionGroup () );
-		parent::setSectionTitle ( $sectionInfo->getSectionTitle () );
-		parent::setFieldInfoArray ( $fieldInfoArray );
+		parent::setOptionGroup($pageInfo->getOptionGroup());
+		parent::setSectionTitle($sectionInfo->getSectionTitle());
+		parent::setFieldInfoArray($fieldInfoArray);
 
-		$editorialReviewLengthTextField = new TextField ( $pageInfo->getOptionNameOfDatabase (),
-				$fieldInfoArray [0] );
-		parent::addField ( $pageInfo, $sectionInfo, $editorialReviewLengthTextField );
+		$editorialReviewLengthTextField = new TextField(
+			$pageInfo->getOptionNameOfDatabase(),
+			$fieldInfoArray[0]
+		);
+		parent::addField($pageInfo, $sectionInfo, $editorialReviewLengthTextField);
 
-		$stringToDeleteTextarea = new TextareaField ( $pageInfo->getOptionNameOfDatabase (),
-				$fieldInfoArray [1] );
-		parent::addField ( $pageInfo, $sectionInfo, $stringToDeleteTextarea );
+		$stringToDeleteTextarea = new TextareaField(
+			$pageInfo->getOptionNameOfDatabase(),
+			$fieldInfoArray[1]
+		);
+		parent::addField($pageInfo, $sectionInfo, $stringToDeleteTextarea);
 
-		$stringToBreakTextarea = new TextareaField ( $pageInfo->getOptionNameOfDatabase (),
-				$fieldInfoArray [2] );
-		parent::addField ( $pageInfo, $sectionInfo, $stringToBreakTextarea );
+		$stringToBreakTextarea = new TextareaField(
+			$pageInfo->getOptionNameOfDatabase(),
+			$fieldInfoArray[2]
+		);
+		parent::addField($pageInfo, $sectionInfo, $stringToBreakTextarea);
 	}
 
-	public function sanitizeParagraphValue($inputedValueMap, &$sanitizedValueMap) {
+	public function sanitizeParagraphValue($inputedValueMap, &$sanitizedValueMap)
+	{
 
 		// sanitizedValueMap：変更するため、配列の参照渡しとする。
-		$this->validateMoreThanZero ( $inputedValueMap );
-		$this->validateExistence ( $inputedValueMap );
-		parent::sanitizeParagraphValue ( $inputedValueMap, $sanitizedValueMap );
+		$this->validateMoreThanZero($inputedValueMap);
+		$this->validateExistence($inputedValueMap);
+		parent::sanitizeParagraphValue($inputedValueMap, $sanitizedValueMap);
 	}
 }
