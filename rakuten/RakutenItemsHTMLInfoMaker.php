54c7de21af6b34a094179fe60fc3405c0d79a753
programming goodsmemo <info.programming@goods-memo.net>
2023-07-09 22:42:24 +0900

    コメント文を削除

--- rakuten/RakutenItemsHTMLInfoMaker.php
+++ rakuten/RakutenItemsHTMLInfoMaker.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
- * Copyright (C) 2018 Goods Memo.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
- */
-
 namespace goodsmemo\rakuten;
 
 use goodsmemo\item\ItemsHTMLInfoMaker;
@@ -41,38 +22,39 @@ require_once GOODS_MEMO_DIR . "rakuten/RakutenResponse.php";
 require_once GOODS_MEMO_DIR . "item/html/ItemHTMLOption.php";
 require_once GOODS_MEMO_DIR . "option/rakuten/RakutenSettingSection.php";
 
-/**
- * Description of RakutenItemsHTMLInfoMaker
- *
- * @author Goods Memo
- */
-class RakutenItemsHTMLInfoMaker implements ItemsHTMLInfoMaker {
-
+class RakutenItemsHTMLInfoMaker implements ItemsHTMLInfoMaker
+{
 	private $commonParameter;
 	private $restParameter;
 	private $searchOption;
 
-	public function __construct(CommonRESTParameter $commonParameter, RESTParameter $restParameter,
-		SearchOption $searchOption) {
+	public function __construct(
+		CommonRESTParameter $commonParameter,
+		RESTParameter $restParameter,
+		SearchOption $searchOption
+	) {
 
 		$this->commonParameter = $commonParameter;
 		$this->restParameter = $restParameter;
 		$this->searchOption = $searchOption;
 	}
 
-	public function requestItemSearch(URLInfo $urlInfo, int $itemCount) {
+	public function requestItemSearch(URLInfo $urlInfo, int $itemCount)
+	{
 
 		$response = RakutenRequest::requestIchibaItemSearch($urlInfo, $this->commonParameter, $this->restParameter);
 		return $response;
 	}
 
-	public function makeItemArray($response, ItemHTMLOption $itemHTMLOption) {
+	public function makeItemArray($response, ItemHTMLOption $itemHTMLOption)
+	{
 
 		$itemArray = RakutenResponse::makeItemArray($response, $itemHTMLOption); // var_dump($itemArray);
 		return $itemArray;
 	}
 
-	public function makeUniqueText(ItemHTMLOption $itemHTMLOption) {
+	public function makeUniqueText(ItemHTMLOption $itemHTMLOption)
+	{
 
 		$operation = $this->searchOption->getOperation();
 		$keyword = $this->restParameter->getKeyword();
@@ -82,5 +64,4 @@ class RakutenItemsHTMLInfoMaker implements ItemsHTMLInfoMaker {
 		$uniqueText = RakutenSettingSection::ID_PREFIX . $operation . $keyword . $numberToDisplay . $cacheExpirationInSeconds;
 		return $uniqueText;
 	}
-
 }
