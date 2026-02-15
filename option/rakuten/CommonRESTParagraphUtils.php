33364de9f18cabe8cea832fa3949e32860c11017
programming goodsmemo <info.programming@goods-memo.net>
2026-02-15 16:20:21 +0900

    設定画面にアクセスキー項目を追加

--- option/rakuten/CommonRESTParagraphUtils.php
+++ option/rakuten/CommonRESTParagraphUtils.php
@@ -1,23 +1,5 @@
 <?php
 
-/*
- * Copyright (C) 2018 Goods Memo.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301 USA
- */
 namespace goodsmemo\option\rakuten;
 
 use goodsmemo\option\field\TextFieldInfo;
@@ -26,28 +8,31 @@ use goodsmemo\option\rakuten\RakutenSettingSection;
 require_once GOODS_MEMO_DIR . "option/field/TextFieldInfo.php";
 require_once GOODS_MEMO_DIR . "option/rakuten/RakutenSettingSection.php";
 
-/**
- * Description of CommonRESTParagraphUtils
- *
- * @author Goods Memo
- */
-class CommonRESTParagraphUtils {
+class CommonRESTParagraphUtils
+{
 	const APPLICATION_ID_ID = RakutenSettingSection::ID_PREFIX . "_application_id_id";
+	const ACCESS_KEY_ID = RakutenSettingSection::ID_PREFIX . "_access_key_id";
 	const AFFILIATE_ID_ID = RakutenSettingSection::ID_PREFIX . "_affiliate_id_id";
 
-	public static function makeFieldInfoArray() {
+	public static function makeFieldInfoArray()
+	{
 
-		$fieldInfoArray = array ();
+		$fieldInfoArray = array();
 
-		$applicationIDFieldInfo = new TextFieldInfo ();
-		$applicationIDFieldInfo->setFieldID ( CommonRESTParagraphUtils::APPLICATION_ID_ID );
-		$applicationIDFieldInfo->setFieldLabel ( '楽天アプリID/デベロッパーID' );
-		array_push ( $fieldInfoArray, $applicationIDFieldInfo );
+		$applicationIDFieldInfo = new TextFieldInfo();
+		$applicationIDFieldInfo->setFieldID(CommonRESTParagraphUtils::APPLICATION_ID_ID);
+		$applicationIDFieldInfo->setFieldLabel('楽天アプリID/デベロッパーID');
+		array_push($fieldInfoArray, $applicationIDFieldInfo);
 
-		$affiliateIDFieldInfo = new TextFieldInfo ();
-		$affiliateIDFieldInfo->setFieldID ( CommonRESTParagraphUtils::AFFILIATE_ID_ID );
-		$affiliateIDFieldInfo->setFieldLabel ( '楽天アフィリエイトID' );
-		array_push ( $fieldInfoArray, $affiliateIDFieldInfo );
+		$accessKeyFieldInfo = new TextFieldInfo();
+		$accessKeyFieldInfo->setFieldID(CommonRESTParagraphUtils::ACCESS_KEY_ID);
+		$accessKeyFieldInfo->setFieldLabel('楽天アプリ/アクセスキー');
+		array_push($fieldInfoArray, $accessKeyFieldInfo);
+
+		$affiliateIDFieldInfo = new TextFieldInfo();
+		$affiliateIDFieldInfo->setFieldID(CommonRESTParagraphUtils::AFFILIATE_ID_ID);
+		$affiliateIDFieldInfo->setFieldLabel('楽天アフィリエイトID');
+		array_push($fieldInfoArray, $affiliateIDFieldInfo);
 
 		return $fieldInfoArray;
 	}
--- option/rakuten/RakutenSettingSection.php
+++ option/rakuten/RakutenSettingSection.php
@@ -1,24 +1,5 @@
 <?php
 
-/*
- * Copyright (C) 2018 Goods Memo.
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301  USA
- */
-
 namespace goodsmemo\option\rakuten;
 
 use goodsmemo\option\AbstractSettingSection;
@@ -52,11 +33,13 @@ require_once GOODS_MEMO_DIR . "option/rakuten/ReviewParagraphUtils.php";
  *
  * @author Goods Memo
  */
-class RakutenSettingSection extends AbstractSettingSection {
+class RakutenSettingSection extends AbstractSettingSection
+{
 
 	const ID_PREFIX = "rakuten";
 
-	public function initSection(PageInfo $pageInfo) {
+	public function initSection(PageInfo $pageInfo)
+	{
 
 		$sectionInfo = new SectionInfo();
 		$sectionInfo->setSectionID("rakuten_section_id");
@@ -90,8 +73,8 @@ class RakutenSettingSection extends AbstractSettingSection {
 		parent::addParagraph($reviewParagraph);
 	}
 
-	public function printSectionInfo() {
+	public function printSectionInfo()
+	{
 		print '楽天アフィリエイトの設定を入力してください。未入力だった場合、初期値を表示します。';
 	}
-
 }
